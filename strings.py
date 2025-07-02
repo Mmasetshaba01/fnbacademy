4f86715e003494a85d0a88f738237a6736cf3380
Mmasetshaba01 <masemola123@icloud.com>
2025-07-02 14:52:37 +0200

    Add files via upload
    module11

---
+++ strings.py
@@ -0,0 +1,10 @@
+#Advanced concepts - strings
+
+message = ' Hello Word! '
+
+print(message.strip()) # Remove the leading whitespaces
+print(message.lower()) # Convvert all characters to lowercase
+print(message.split(',')) # Split the string into a list based on the comma
+
+#upper methos
+#replace method
