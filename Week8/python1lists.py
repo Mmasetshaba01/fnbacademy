81c1c3cc4b2eded6d754281ef442a1709e9f67e4
Mmasetshaba01 <masemola123@icloud.com>
2025-07-11 14:31:11 +0200

    Add files via upload

---
+++ Week8/python1lists.py
@@ -0,0 +1,8 @@
+
+fruits = ["apple","bannana","cherry"]
+
+print(fruits[0])
+
+fruits[1] = "blueberry"
+
+print(fruits)
---
+++ Week8/pythonlist2.py
@@ -0,0 +1,15 @@
+
+fruits = ["apple","bannana","cherry"]
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits)
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
+
